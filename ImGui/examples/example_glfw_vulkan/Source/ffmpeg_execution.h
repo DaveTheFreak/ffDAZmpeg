df5bef5e7c93274f39e51abedad88afd2a7e5014
Dave Scholze <realdavethefreak@googlemail.com>
2026-03-08 15:00:18 +0100

    ffDAZmpeg_0.1.0
    - small bare-bones app
       - simple dummy conversion
       - simple UI
       - settings save system

---
+++ ImGui/examples/example_glfw_vulkan/Source/ffmpeg_execution.h
@@ -0,0 +1,72 @@
+﻿// Copyright @Dave Scholze 2026
+
+#pragma once
+
+#include "save_system.h"
+
+/** This creates the actual command(s) for ffmpeg. */
+struct ffmpeg_execution
+{
+// METHODS =============================================================================================================
+#pragma region Methods
+
+    const char* get_ffmpeg_arguments(const app_settings &settings)
+    {
+        ffmpeg_command.clear();
+
+        // Input format (.png or .tiff) --------------------------------------------------------------------------------
+
+        std::filesystem::path input_file =
+            std::filesystem::path(settings.input_path) / current_filename;
+
+        // Output format (.png or .tiff) -------------------------------------------------------------------------------
+
+        std::filesystem::path output_file =
+            std::filesystem::path(settings.output_path) / std::filesystem::path(current_filename).stem();
+        output_file += ".avif";
+
+        // Effects -----------------------------------------------------------------------------------------------------
+
+        // ...
+
+        // Actual conversion -------------------------------------------------------------------------------------------
+
+        ffmpeg_command = "-y -i \"" + input_file.string() + "\" -c:v av1_nvenc \"" + output_file.string() + "\"";
+
+        // Return ------------------------------------------------------------------------------------------------------
+        return ffmpeg_command.c_str();
+    }
+
+    void Run(app_settings settings)
+    {
+        ffmpeg_path = std::string(settings.ffmpeg_path) + "\\ffmpeg.exe";
+
+        for (const auto& entry : std::filesystem::directory_iterator(settings.input_path))
+        {
+            if (entry.is_regular_file() && entry.path().extension() == ".png")
+            {
+                current_filename = entry.path().filename().string();
+
+                std::string command = "\"" + ffmpeg_path + "\" " + get_ffmpeg_arguments(settings);
+
+                // Added /wait to prevent launching all instances simultaneously and overloading the NVENC encoder.
+                std::string finalCmd = "start \"ffmpeg_test\" /wait " + command;
+
+                std::system(finalCmd.c_str());
+            }
+        }
+    }
+
+#pragma endregion
+
+// VARIABLES ===========================================================================================================
+#pragma region Variables
+
+    std::string ffmpeg_path;
+    std::string ffmpeg_command;
+    std::string current_filename;
+
+#pragma endregion
+
+};
+
---
+++ ImGui/examples/example_glfw_vulkan/Source/save_system.h
@@ -0,0 +1,94 @@
+﻿// Copyright @Dave Scholze 2026
+
+#pragma once
+
+#include <windows.h>
+#include <filesystem>
+#include <fstream>
+#include <map>
+#include <string>
+
+struct app_settings
+{
+    // Folders ---------------------------------------------------------------------------------------------------------
+    char input_path[512] =
+        R"(C:\)";
+    char output_path[512] =
+        R"(C:\)";
+
+    // Format ----------------------------------------------------------------------------------------------------------
+
+        /** "AVIF", "HEIC", "JPEG XL", "JPEG" */
+        int8_t selected_image_format = 0;
+
+        /** "SDR", "HDR" */
+        int8_t selected_dynamic_range = 0;
+
+        /** 8, 10 */
+        int8_t selected_bit_depth_index = 1;
+
+    // Effects ---------------------------------------------------------------------------------------------------------
+    float chromatic_aberration {0.002f};
+    bool enable_image_enhancement {true};
+    char logo_path[512] =
+        R"(C:\)";
+
+    // Encoder ---------------------------------------------------------------------------------------------------------
+    int8_t selected_encoder_option = 0;
+
+    // Settings --------------------------------------------------------------------------------------------------------
+    char ffmpeg_path[512] =
+        R"(C:\)";
+
+};
+
+struct save_system
+{
+private:
+
+    static std::string get_settings_path()
+    {
+        char buffer[MAX_PATH];
+        GetModuleFileNameA(NULL, buffer, MAX_PATH);
+        std::filesystem::path exe_path(buffer);
+        return (exe_path.parent_path() / "ffDAZmpeg.bin").string();
+    }
+
+public:
+
+    void save_settings() const
+    {
+        std::ofstream file(get_settings_path(), std::ios::binary);
+        if (file.is_open())
+        {
+            file.write(reinterpret_cast<const char*>(&settings), sizeof(app_settings));
+        }
+    }
+
+    void load_settings()
+    {
+        std::ifstream file(get_settings_path(), std::ios::binary);
+        if (file.is_open())
+        {
+            file.read(reinterpret_cast<char*>(&settings), sizeof(app_settings));
+        }
+        else
+        {
+            save_settings();
+        }
+    }
+
+    void revert_settings()
+    {
+        settings = app_settings();
+        save_settings();
+    }
+
+    /** Current App settings. */
+    app_settings settings;
+
+    // TODO profiles
+    std::map<std::string, app_settings> profiles;
+
+};
+
