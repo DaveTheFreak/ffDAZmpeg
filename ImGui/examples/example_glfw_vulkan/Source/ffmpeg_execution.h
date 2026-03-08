e526c47d03002911c4f89de0c76b2aae452affca
Dave Scholze <realdavethefreak@googlemail.com>
2026-03-08 19:49:58 +0100

    ffDAZmpeg_0.1.3
    - added true nvenc avif encoding
       - SDR 8
       - SDR 10
       - HDR 10 with curve

--- ImGui/examples/example_glfw_vulkan/Source/ffmpeg_execution.h
+++ ImGui/examples/example_glfw_vulkan/Source/ffmpeg_execution.h
@@ -27,17 +27,68 @@ struct ffmpeg_execution
             std::filesystem::path(settings.output_path) / std::filesystem::path(current_filename).stem();
         output_file += ".avif";
 
-        // Encoder Settings --------------------------------------------------------------------------------------------
-
-        std::string encoding = R"(-c:v av1_nvenc -rc constqp -qp 8 -preset p7 -pix_fmt p010le)";
-
         // Effects -----------------------------------------------------------------------------------------------------
 
-        // ...
+        std::string effects {""};
+
+        // Encoder Settings --------------------------------------------------------------------------------------------
+
+        std::string encoding;
+        std::string dynamic_range;
+
+        switch (settings.selected_image_format)
+        {
+            case 0: // AVIF
+            {
+                std::string pix_fmt = (settings.selected_bit_depth_index == 0) ? "yuv420p" : "p010le";
+
+                encoding = "-c:v av1_nvenc -rc constqp -qp 8 -preset p7 -pix_fmt " + pix_fmt;
+
+                if (settings.selected_bit_depth_index == 0) // 8 bit (supports only SDR)
+                {
+                    dynamic_range = "-color_range pc -colorspace bt709 -color_primaries bt709 -color_trc iec61966-2-1";
+                }
+                else // 10 bit
+                {
+                    if (settings.selected_dynamic_range == 0) // SDR
+                    {
+                        dynamic_range =
+                            "-color_range pc -colorspace bt709 -color_primaries bt709 -color_trc iec61966-2-1";
+                    }
+                    else // HDR
+                    {
+                        dynamic_range = "-vf \"format=gbrp16le,curves=all='0/0 0.1/0.09 0.5/0.45 0.8/0.75 1/1',";
+                        dynamic_range += "colorchannelmixer=rr=0.90:rg=0.05:rb=0.05:gr=0.05:gg=0.90:gb=0.05:br=0.05:bg=0.05:bb=0.90,";
+                        dynamic_range += "zscale=pin=1:rin=1:tin=iec61966-2-1:p=9:m=9:r=0:t=16:npl=1000\" ";
+
+                        dynamic_range += "-color_range tv -colorspace bt2020nc -color_primaries bt2020 -color_trc smpte2084 ";
+                        dynamic_range += "-bsf:v av1_metadata=chroma_sample_position=1:color_range=1:color_primaries=9:transfer_characteristics=16:matrix_coefficients=9";
+                    }
+                }
+                break;
+            }
+            case 1: // HEIC
+            {
+                break;
+            }
+            case 3: // JPEG XL
+            {
+                break;
+            }
+            case 4: // JPEG
+            {
+                break;
+            }
+            default: break;
+        }
 
         // Actual conversion -------------------------------------------------------------------------------------------
 
-        ffmpeg_command = "-y -i \"" + input_file.string() + "\" " + encoding + " \"" + output_file.string() + "\"";
+        ffmpeg_command = "-y -i \""
+        +   input_file.string() + "\" "
+        +   encoding + " "
+        +   dynamic_range + " \""
+        +   output_file.string() + "\"";
 
         // Return ------------------------------------------------------------------------------------------------------
         return ffmpeg_command.c_str();
@@ -69,34 +120,6 @@ struct ffmpeg_execution
 
                     // Final string follows your original working quote pattern
                     std::string finalCmd = "\"" + command + redirection + "\"";
-
-                    std::system(finalCmd.c_str());
-                }
-            }
-        }).detach();
-    }
-
-    void RunAlwaysFfmpegLog(app_settings settings)
-    {
-        // Execute in a detached background thread to prevent the DearImGui app from freezing
-        std::thread([this, settings]()
-        {
-            std::string local_ffmpeg_path = std::string(settings.ffmpeg_path) + "\\ffmpeg.exe";
-
-            for (const auto& entry : std::filesystem::directory_iterator(settings.input_path))
-            {
-                if (entry.is_regular_file()
-                    && (entry.path().extension() == ".png" || entry.path().extension() == ".tiff"))
-                {
-                    current_filename = entry.path().filename().string();
-
-                    std::string command = "\"" + local_ffmpeg_path + "\" " + get_ffmpeg_arguments(settings);
-
-                    std::string log_file = std::string(settings.output_path) + "\\ffmpeg_log.txt";
-
-                    // Wrap the entire command in an extra set of quotes for cmd.exe parsing.
-                    // Use 2>> to append logs for all files in the batch.
-                    std::string finalCmd = "\"" + command + " 2>> \"" + log_file + "\"\"";
 
                     std::system(finalCmd.c_str());
                 }
